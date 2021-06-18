3f7bbd7c9787aa44ea769d08086e23ffcd3d5654
tienpham2009 <tiendx2009@gmail.com>
2021-06-18 22:26:51 +0700

    manage customer

---
+++ database/seeders/CustomerTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Psy\Util\Str;
+
+class CustomerTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $dataArray = [];
+        for ($i = 0; $i < 20; $i++) {
+            array_push($dataArray, [
+                'name' => \Illuminate\Support\Str::random(10),
+                'dob' => date("Y-m-d", mt_rand(1, time())),
+                'email' => \Illuminate\Support\Str::random(10) . '@gmail.com',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+
+            ]);
+        }
+        DB::table('customer')->insert($dataArray);
+    }
+}
